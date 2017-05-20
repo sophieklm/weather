c478aacddfe32f955a0d2d9104455be746690f58
Sophie Mann <sklm@live.co.uk>
2017-05-20 13:41:36 +0100

    https

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ $( document ).ready(function() {
   }
 
   function showLocation(latitude, longitude) {
-    $.get('http://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true', function(data){
+    $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true', function(data){
        $('#current-location').text(data.results[0].address_components[3].long_name);
     });
   }
